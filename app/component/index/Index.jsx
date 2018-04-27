ed84b9a6483ea8fb2465192d30c74de952da273c
zhalice2011 <zhalice2012@gmail.com>
2018-04-28 07:38:42 +0800

    webpack配置optimization，提取第三方包及公共组件

--- app/component/index/Index.jsx
+++ app/component/index/Index.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import '../../public/css/common.pcss';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 import '../../public/css/index.pcss';
+
 
 class Index extends React.Component {
     render() {
         return (
             <div className="cont">
-                <div className="top">
-                    <div>这是头部</div>
-                    <div>
-                        <i className="logo"/>
-                    </div>
-                    <div className="nav">
-                        <a href="/index.html">首页</a> <a href="/shop.html">商城</a>
-                    </div>
-                </div>
+                <Header />
                 <div className="index">
                     这是首页
                 </div>
-                <div className="bottom">
-                    这是底部
-                </div>
+                <Footer />
             </div>
         );
     }
--- app/component/shop/Index.jsx
+++ app/component/shop/Index.jsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import '../../public/css/common.pcss';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 import '../../public/css/shop.pcss';
 
 class Index extends React.Component {
     render() {
         return (
             <div className="cont">
-                <div className="top">
-                    <div>这是头部</div>
-                    <div>
-                        <i className="logo"/>
-                    </div>
-                    <div className="nav">
-                        <a href="/index.html">首页</a> <a href="/shop.html">商城</a>
-                    </div>
-                </div>
+                <Header />
                 <div className="shop">
                     这是商城
                 </div>
-                <div className="bottom">
-                    这是底部
-                </div>
+                <Footer />
             </div>
         );
     }
